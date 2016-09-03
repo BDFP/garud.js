8b382baa79a0e671df13512a21da029f4bb789ca
rag594 <raghavrastogi594@gmail.com>
2016-09-03 20:06:52 +0530

    add example

---
+++ example/app/Login.js
@@ -0,0 +1,31 @@
+(function () {
+	var loginValidations = {
+		'#email': [ Garud.Util.checkNotNull ],
+		'#password': [ Garud.Util.checkNotNull ]
+	}
+
+	// array or object' if array then domelement o/w object
+	// route post location
+	// successCb
+	// errorCb
+	var doLogin = Garud.HTTP.Post(
+		['#email', '#password'], 
+		'/login', 
+		function (response) {
+			//key, value
+			Garud.Data.saveToMemory('loginData', response);
+
+			//targetLocation
+			Garud.transit('/home');
+		}, 
+		function (reason) {
+			//string reason
+			Garud.Util.ShowError(reason);
+		}
+	);
+
+	var Login = new GarudComponent();
+
+	//querySelector, validationObject, actionFunc
+	Login.onClick('#login-btn', loginValidations, doLogin)
+})();
