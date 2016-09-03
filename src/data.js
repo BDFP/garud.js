5f42d4286cbb9e9a51432e503ce6db293302f22e
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2016-09-03 21:28:21 +0530

    add data

---
+++ src/data.js
@@ -0,0 +1,20 @@
+Garud.Data = function () {
+    this.memDB = {};
+    this.varObservable = {};
+};
+
+Garud.Data.prototype.saveToMemory = function (key, value) {
+    memDB[key] = value;
+    for (var changeFunc of this.varObservable[key]) {
+        changeFunc(value);
+    }
+}
+
+Garud.Data.prototype.observe = function (key, onChangeCb) {
+    varObservable[key] = varObservable[key] || [];
+    varObservable[key].push(onChangeCb);
+}
+
+Garud.Data.prototype.getFromMemory = function (key) {
+    return memDB[key];
+}
